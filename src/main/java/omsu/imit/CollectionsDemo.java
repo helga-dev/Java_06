499e1a0d95d3e36254bede03908941137eb3563b
Olga Belozerova <basyanet@gmail.com>
2022-02-10 13:16:34 +0600

    done

---
+++ src/main/java/omsu/imit/CollectionsDemo.java
@@ -0,0 +1,12 @@
+package omsu.imit;
+
+/**
+ * Hello world!
+ *
+ */
+public class CollectionsDemo {
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/omsu/imit/CollectionsDemoTest.java
@@ -0,0 +1,20 @@
+package omsu.imit;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple CollectionsDemo.
+ */
+public class CollectionsDemoTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
